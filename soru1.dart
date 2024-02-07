ee933020848c8b57d191138e060fde0a34d92bab
muhammettalhakaymak23214 <talha.kaymak@bil.omu.edu.tr>
2024-02-07 16:51:04 +0300

    soru1 eklendi

---
+++ soru1.dart
@@ -0,0 +1,27 @@
+/*
+Soru:
+Size bir sayı dizisi ve 'k' sayısı veriliyor. Bu dizideki herhangi iki sayının toplamının 'k' olup olmadığını bulan bir program yazınız.
+Örnek:
+Liste = [10, 15, 3, 7]
+k = 17 için 10 ve 7 sayılarının toplamı 17 ediyor.
+*/
+
+void main() {
+  List<int> sayiListesi = [10, 15, 3, 2];
+  int k = 17;
+  print(hesapla(k: k, sayiListesi: sayiListesi));
+}
+
+bool hesapla({required int k, required List<int> sayiListesi}) {
+  int sayac = 0;
+  List<int> ikinciListe = [];
+  while (sayac < sayiListesi.length) {
+    if (ikinciListe.contains(sayiListesi[sayac])) {
+      return true;
+    }
+    ikinciListe.add(k - sayiListesi[sayac]);
+    sayac++;
+  }
+
+  return false;
+}
